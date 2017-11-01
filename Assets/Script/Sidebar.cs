b0473fcc22507e0c18a386965f58c54dd0de416f
p4r4d0x <alexanderdinney@gmail.com>
2017-11-01 11:48:10 -0400

    Added sidebar

--- Assets/Script/Sidebar.cs
+++ Assets/Script/Sidebar.cs
@@ -1,16 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Sidebar : MonoBehaviour {
+public class Sidebar : MonoBehaviour
+{
+    public Button expandButton;
+    public enum State {
+        closed, open, opening, closing
+    }
+    public State currentState;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Animator anim;
+
+    // Use this for initialization
+    void Start() {
+        anim = GetComponent<Animator> ();
+    }
+
+    void Awake () {
+        currentState = State.closed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ExpandMenu ()
+    {
+        if (currentState == State.closed) {
+            anim.SetTrigger ("Open");
+            expandButton.GetComponent<RectTransform> ().rotation = Quaternion.Euler (0, 0, -90f);
+            currentState = State.open;
+        } else if (currentState == State.open) {
+            anim.SetTrigger ("Close");
+            expandButton.GetComponent<RectTransform> ().rotation = Quaternion.Euler (0, 0, 90f);
+            currentState = State.closed;
+        }
+    }
 }
